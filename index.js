861f4efc9bb4701d54032222f37740c7f89d794a
zerookrash <zerookrash@me.com>
2019-08-02 20:38:57 -0500

    cambio

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config();
 require('./database');
 const { GraphQLServer } = require('graphql-yoga');
 const { makeExecutableSchema } = require("graphql-tools");
